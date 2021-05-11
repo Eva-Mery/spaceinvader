325e422059ce6d08ce03bc93630eec9f97523b46
Éva <eva.mery@etu.unilim.fr>
2021-05-11 17:29:28 +0200

    sauvegarde de fin de séance

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -44,12 +44,6 @@ public class SpaceInvaders {
 		return vaisseau!=null;
 	}
 
-	public void positionnerUnNouveauVaisseau(int x, int y) {
-		if (  !estDansEspaceJeu(x, y) )
-			throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
-		vaisseau = new Vaisseau(x, y); 
-	}
-
 	private boolean estDansEspaceJeu(int x, int y) {
 		return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
 	}
@@ -61,4 +55,15 @@ public class SpaceInvaders {
 	public void deplacerVaisseauVersLaGauche() {
 		if (vaisseau.abscisse() > (0)) vaisseau.seDeplacerVersLaGauche();
 	}
+	   
+
+	public void positionnerUnNouveauVaisseau(int longueur, int hauteur, int x, int y) {
+		if (!estDansEspaceJeu(x, y))
+			throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
+
+		vaisseau = new Vaisseau(longueur, hauteur);
+		vaisseau.positionner(x, y);
+	}
+
+
 }
